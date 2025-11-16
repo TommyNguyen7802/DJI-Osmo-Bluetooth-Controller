cfb68cdb7ce4cc58226dbde7299c3d90a1da6d32
Mason Jennings <masonj@csu.fullerton.edu>
2025-11-15 17:32:05 -0800

    moved camera actions to "dji_actions"

--- DJI Scripts/dji_actions.py
+++ DJI Scripts/dji_actions.py
@@ -1,4 +1,31 @@
-'''
-TODO: 
-**Future location of basic camera actions @mason
-'''
+"""
+Contains the camera actions used in 'entry_keyboard_control
+"""
+
+from dji_commands import build_record_command
+from dji_protocol import build_frame, next_seq
+from dji_structs import build_camera_mode_switch
+
+
+async def start_recording(ble, device_id):
+    payload = build_record_command(device_id, start=True)
+    frame = build_frame(0x1D, 0x03, 0x00, payload, next_seq())
+    await ble.write(frame)
+
+
+async def stop_recording(ble, device_id):
+    payload = build_record_command(device_id, start=False)
+    frame = build_frame(0x1D, 0x03, 0x00, payload, next_seq())
+    await ble.write(frame)
+
+
+async def switch_mode_video(ble, device_id):
+    payload = build_camera_mode_switch(device_id, 0x01)
+    frame = build_frame(0x1D, 0x04, 0x00, payload, next_seq())
+    await ble.write(frame)
+
+
+async def switch_mode_photo(ble, device_id):
+    payload = build_camera_mode_switch(device_id, 0x05)
+    frame = build_frame(0x1D, 0x04, 0x00, payload, next_seq())
+    await ble.write(frame)
--- DJI Scripts/entry_keyboard_control.py
+++ DJI Scripts/entry_keyboard_control.py
@@ -31,30 +31,7 @@ from dji_ble import DJIBLE
 from dji_commands import build_connection_request, build_record_command
 from dji_protocol import build_frame, next_seq
 from dji_structs import build_status_subscription, build_camera_mode_switch
-
-
-async def start_recording(ble, device_id):
-    payload = build_record_command(device_id, start=True)
-    frame = build_frame(0x1D, 0x03, 0x00, payload, next_seq())
-    await ble.write(frame)
-
-
-async def stop_recording(ble, device_id):
-    payload = build_record_command(device_id, start=False)
-    frame = build_frame(0x1D, 0x03, 0x00, payload, next_seq())
-    await ble.write(frame)
-
-
-async def switch_mode_video(ble, device_id):
-    payload = build_camera_mode_switch(device_id, 0x01)
-    frame = build_frame(0x1D, 0x04, 0x00, payload, next_seq())
-    await ble.write(frame)
-
-
-async def switch_mode_photo(ble, device_id):
-    payload = build_camera_mode_switch(device_id, 0x05)
-    frame = build_frame(0x1D, 0x04, 0x00, payload, next_seq())
-    await ble.write(frame)
+from dji_actions import *
 
 
 def on_press_wrapper(ble, device_id, asyncio_running_loop):
