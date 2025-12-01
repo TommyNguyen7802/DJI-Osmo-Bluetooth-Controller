974c714776b00cdf2441becc5a1ccff47243cefa
Mason Jennings <masonj@csu.fullerton.edu>
2025-12-01 15:21:14 -0800

    update sleep after connection to 0.5

--- DJI Scripts/ssh_friendly_entry_keyboard_control.py
+++ DJI Scripts/ssh_friendly_entry_keyboard_control.py
@@ -78,7 +78,7 @@ async def main():
     frame = build_frame(0x00, 0x19, 0x00, payload, next_seq())
     await ble.write(frame)
 
-    await asyncio.sleep(2)
+    await asyncio.sleep(0.5)
 
     while True:
         key = get_key()
