77cbed7890987884f139b51f9a3c8c3d866025d6
Mason Jennings <masonj@csu.fullerton.edu>
2025-12-01 15:14:55 -0800

    initial commit

---
+++ DJI Scripts/keyboard_control.py
@@ -0,0 +1,12 @@
+"""
+Functions to control the camera by keyboard.
+"""
+from dji_actions import (
+    start_recording,
+    stop_recording,
+    switch_mode_video,
+    switch_mode_photo,
+)
+
+def keyboard_control():
+    print("Hello world!")
