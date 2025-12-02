8176c5b9f44a845739478fc0d510ba29b8e43784
Mason Jennings <masonj@csu.fullerton.edu>
2025-12-02 15:10:23 -0800

    added sleep functions

--- DJI Scripts/compiled_main.py
+++ DJI Scripts/compiled_main.py
@@ -4,14 +4,19 @@ import asyncio
 from uhubctl import disable_hub, enable_hub
 from keyboard_control import keyboard_control
 from transfer_video import transfer_new_videos
+from time import sleep
 
 async def main():
     # Disable all usb ports on Raspberry Pi 5
     disable_hub(2)
     disable_hub(4)
 
+    sleep(0.5)
+
     # Camera logic
     asyncio.run(keyboard_control())
+
+    sleep(0.5)
 
     # Enable all usb ports on Raspberry Pi 5
     enable_hub(2)
