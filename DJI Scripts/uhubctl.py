f7f0afb5ce9272076feb43503439445adc9d9b83
masonj5002 <masonj@csu.fullerton.edu>
2026-02-05 13:11:30 -0800

    add optional print statements

--- DJI Scripts/uhubctl.py
+++ DJI Scripts/uhubctl.py
@@ -6,6 +6,7 @@ def disable_hub(location):
     # print("Running:", " ".join(cmd))
     try:
         output = subprocess.run(cmd, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        # print(output)
     except subprocess.CalledProcessError as e:
         print(f"Error disabling hub {location}:", e)
 
