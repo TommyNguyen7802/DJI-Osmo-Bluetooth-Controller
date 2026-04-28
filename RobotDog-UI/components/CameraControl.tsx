2edffc2ad7aa8c63c8dcacbbd7b51508e5873668
masonj5002 <masonj@csu.fullerton.edu>
2026-04-28 12:18:39 -0700

    back to ip

--- RobotDog-UI/components/CameraControl.tsx
+++ RobotDog-UI/components/CameraControl.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const API_BASE = "http://localhost:8010"
+const API_BASE = "http://192.168.0.101:8010"
 
 interface CameraControlProps {
   activeButtons: { [key: string]: boolean };
@@ -22,13 +22,13 @@ export const CameraControl: React.FC<CameraControlProps> = ({ activeButtons, onB
     onButtonPress(key, active);
     if (!active) return;
 
-    if (key === "a") fetch("http://localhost:8010/camera/start", { method: "POST" });
-    if (key === "b") fetch("http://localhost:8010/camera/stop", { method: "POST" });
-    if (key === "x") fetch("http://localhost:8010/camera/video", { method: "POST" });
-    if (key === "y") fetch("http://localhost:8010/camera/photo", { method: "POST" });
-    if (key === "c") fetch("http://localhost:8010/camera/transfer", { method: "POST" });
-    if (key === "d") fetch("http://localhost:8010/camera/setup", { method: "POST" });
-    if (key === "e") fetch("http://localhost:8010/camera/shutdown", { method: "POST" });
+    if (key === "a") fetch("http://192.168.0.101:8010/camera/start", { method: "POST" });
+    if (key === "b") fetch("http://192.168.0.101:8010/camera/stop", { method: "POST" });
+    if (key === "x") fetch("http://192.168.0.101:8010/camera/video", { method: "POST" });
+    if (key === "y") fetch("http://192.168.0.101:8010/camera/photo", { method: "POST" });
+    if (key === "c") fetch("http://192.168.0.101:8010/camera/transfer", { method: "POST" });
+    if (key === "d") fetch("http://192.168.0.101:8010/camera/setup", { method: "POST" });
+    if (key === "e") fetch("http://192.168.0.101:8010/camera/shutdown", { method: "POST" });
   };
 
   const handleClickButton = (key: string) => {
